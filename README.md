5e211b57781bbc880bf424da5c1eb7fedde72cb0
Andrew van Weerden <ando.vanweerden@gmail.com>
2024-11-03 07:05:05 +0800

    Add extremely basic study loop and updated README

--- README.md
+++ README.md
@@ -2,7 +2,17 @@
 A CLI Python program for drilling coding problems 
 
 ## TODO:
-- [ ] Create MVP (flat directory and simple 1-3 evaluation)
+- [ ] Create MVP: Loop that displays random problem when you press button
+- [ ] Add index that tracks metadata
+    * Updated when program run (add missing files and update values that are different)
+    * Structure: JSON key-value where key is filename with props:
+        subject
+        path
+        last viewed
+        failcount
+        okcount
+        easycount
+- [ ] Write selection algorithm: difficulty, last see, then random
 - [ ] Allow selection of subject
 - [ ] Add way to input solution
 - [ ] Add automated testing!! Simple input => expected output (steal from python practice thing)
--- main.py
+++ main.py
@@ -3,6 +3,10 @@
 import sys
 from os import listdir
 from os.path import isfile, join
+import importlib.util
+import keyboard
+
+
 '''
 File structure: JSON with the following props
 subject
@@ -25,14 +29,14 @@ easycount
 
 # SET WORKING DIRECTORY
 working_dir = sys.argv[1]
+indexfilename = "._index"
+
+filenames = [f for f in listdir(working_dir) if isfile(join(working_dir, f)) and f != "._index"]
 
 # INIT CHECKS
-
-filelist = [f for f in listdir(working_dir) if isfile(join(working_dir, f))]
-
 # Check for index file. If not, create it in working_directory
-with open(f"{working_dir}/._index", 'w') as f:
-    for f in filelist:
+# with open(f"{working_dir}/._index", 'w') as f:
+#     for f in filelist:
     # Add any new files to the .index JSON file
     # Update properties of each one if different
 
@@ -40,7 +44,40 @@ with open(f"{working_dir}/._index", 'w') as f:
 # Read from index file 
 
 # STUDY LOOP
-# Loop until all problems have been seen in that session OR user selects to stop:
+keep_studying = True
+index = 0
+problem_count = len(filenames)
+while keep_studying:
+    filename = filenames[index]
+    filepath = f"{working_dir}/{filename}"
+    spec = importlib.util.spec_from_file_location(f"module.{filename}", filepath)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    prompt = getattr(module, 'prompt', None)
+    solution = getattr(module, 'solution', None)
+
+    print(prompt)
+    print("Press any key to see solution")
+    # BUG: This keyboard read is also applied to the (y/n) below
+    keyboard.read_event()
+    print("SOLUTION")
+    print(solution)
+    
+    # If no problems left, notify user and exit
+    index += 1
+    print(f"problem_count: {problem_count}")
+
+    print(f"index: {index}")
+    if index >= problem_count:
+        print("You've studied all the problems!")
+        keep_studying = False
+    else:
+        # BUG: The above keyboard event seems to "count" here 
+        res = input("Keep studying? (y/n)")
+        keep_studying = res.lower() == 'y' or res.lower() == 'yes'
+        if keep_studying:
+            print ("==================")
+
 # SELECT FILE
     # 
 # PARSE FILE
