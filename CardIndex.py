be7a5ac8284e6b9c7aee1a1cc6f1fc6c079fc662
Andrew van Weerden <ando.vanweerden@gmail.com>
2024-11-29 15:13:32 +0800

    Add CardIndex class, refactor methods, and delete utilities.py

---
+++ CardIndex.py
@@ -0,0 +1,47 @@
+import yaml
+from datetime import datetime
+
+class CardIndex():
+    def __init__(self, path, file_names):
+        self.index_dict = {}
+        self.file_list = file_names
+        self.index_filepath = path
+
+        try:
+            with open(self.index_filepath, 'r') as file:
+                self.index_dict = yaml.safe_load(file) or {}
+        except FileNotFoundError:
+            # TODO: Create new file? 
+            self.index_dict = {}
+
+        self.add_new_files()
+    
+    def add_new_files(self):
+        for filename in self.file_list:
+            if filename not in self.index_dict:
+                self.index_dict[filename] = {
+                    'last_seen': None,
+                    'ease': None,
+                    'completion_count': 0
+                }
+
+        with open(self.index_filepath, 'w') as index_file:
+            yaml.dump(self.index_dict, index_file, default_flow_style=False, sort_keys=False)
+
+    def update_card(self, filename, ease_rating):
+        completion_count = self.index_dict[filename].get("completion_count", 0) + 1
+        previous_ease = self.index_dict[filename].get("ease", None)
+        if previous_ease == None:
+            previous_ease = ease_rating
+
+        updated_ease = int(round((float(previous_ease) + float(ease_rating)) / 2, 0))
+
+        self.index_dict[filename]["last_seen"] = datetime.now()
+        self.index_dict[filename]["completion_count"] = completion_count
+        self.index_dict[filename]["ease"] = updated_ease
+
+        self.write_to_file()
+
+    def write_to_file(self):
+        with open(self.index_filepath, 'w') as index_file:
+            yaml.dump(self.index_dict, index_file, default_flow_style=False, sort_keys=False)
--- main.py
+++ main.py
@@ -2,66 +2,102 @@
 
 import sys
 import os
+from os import listdir
+from os.path import isfile, join
 import importlib.util
 import keyboard
 import yaml
 import time
-from utilities import * 
+from CardIndex import CardIndex
+import random
+from enum import Enum
 
 verbose = True
 
-# INITTIALISE
-working_dir = get_working_directory()
-indexfilepath = f"{working_dir}/._index"
+class CardCategory(Enum):
+    NEW = 0
+    HARD = 1
+    NORMAL = 2
 
-prompts = get_prompts(working_dir)
 
-clear_terminal_screen()
+def get_working_directory():
+    options = {}
+    with open("config.yaml", 'r') as stream:
+        options = yaml.safe_load(stream)
+    return options["root-directory"]
 
-# BEGIN STUDY LOOP
-keep_studying = True
-index = 0
-problem_count = len(prompts)
-while keep_studying:
-    filename = prompts[index]
-    filepath = f"{working_dir}/{filename}"
-    spec = importlib.util.spec_from_file_location(f"module.{filename}", filepath)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    subject = getattr(module, 'subject', None)
-    prompt = getattr(module, 'prompt', None)
-    solution = getattr(module, 'solution', None)
+def clear_terminal_screen():
+    # NOTE: This works for Linux. Windows works differently
+    clear = lambda: os.system('clear')
+    clear()
 
-    print("\n================ QUESTION ================\n")
-    if verbose:
-        print("DEBUG <filename>:", filename)
+def card_sort_key(card):
+    '''Returns a tuple containing key for sorting cards.'''
+    is_new = card[1]["last_seen"] is None or card[1]["ease"] is None
+    last_seen = datetime.min if is_new else card[1]["last_seen"]
+    ease = 5 if card[1]["ease"] is None else card[1]["ease"]
 
-    print("Subject:", subject.upper())
-    # TODO: Print difficulty here
-    print(prompt)
-    confirm_see_solution = input("\nPress any key to see solution")
+    category = CardCategory.NEW.value if is_new else CardCategory.HARD.value if ease < 5 else CardCategory.NORMAL.value
+
+    return (category, ease, last_seen)
+
+def main():
+    # INITTIALISE
+    working_dir = get_working_directory()
+    indexfilepath = f"{working_dir}/._index"
+    filenames = [f for f in listdir(working_dir) if isfile(join(working_dir, f)) and f != "._index"]
     
-    print("\n================ SOLUTION ================\n")
-    print(solution)
-    print("\n==========================================\n")
-    
-    # GET DIFFICULTY
-    difficulty_input = input("How difficult was that? (0-10 where 0 is impossible and 10 is trivial) ")
-    while not difficulty_input.isnumeric() and int(difficulty_input) not in range(0,11):
-        difficulty_input = input("That is not an option. Please a number between 1 and 10 (0: impossible and 10: trivial))" )
-    update_metadata(indexfilepath, filename, int(difficulty_input))
+    card_index = CardIndex(indexfilepath, filenames)
+    prompts = list(dict(sorted(card_index.index_dict.items(), key=card_sort_key)))
 
-    # If no problems left, notify user and exit
-    index += 1
-    if index >= problem_count:
-        print("You've studied all the problems!")
-        keep_studying = False
-    else:
-        print(f"{problem_count - index+1} problems left")
-        res = input("Press (n/N) to stop")
-        keep_studying = res.lower() != 'n' and res.lower() != 'no'
-        if keep_studying:
-            clear_terminal_screen()
+    clear_terminal_screen()
+
+    # BEGIN STUDY LOOP
+    keep_studying = True
+    index = 0
+    problem_count = len(prompts)
+    while keep_studying:
+        filename = prompts[index]
+        filepath = f"{working_dir}/{filename}"
+        spec = importlib.util.spec_from_file_location(f"module.{filename}", filepath)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        subject = getattr(module, 'subject', None)
+        prompt = getattr(module, 'prompt', None)
+        solution = getattr(module, 'solution', None)
+
+        print("\n================ QUESTION ================\n")
+        if verbose:
+            print("DEBUG <filename>:", filename)
+
+        print("Subject:", subject.upper())
+        # TODO: Print difficulty here
+        print(prompt)
+        confirm_see_solution = input("\nPress any key to see solution")
+        
+        print("\n================ SOLUTION ================\n")
+        print(solution)
+        print("\n==========================================\n")
+        
+        # GET DIFFICULTY
+        difficulty_input = input("How difficult was that? (0-10 where 0 is impossible and 10 is trivial) ")
+        while not difficulty_input.isnumeric() and int(difficulty_input) not in range(0,11):
+            difficulty_input = input("That is not an option. Please a number between 1 and 10 (0: impossible and 10: trivial))" )
+        card_index.update_card(filename, int(difficulty_input))
+
+        # If no problems left, notify user and exit
+        index += 1
+        if index >= problem_count:
+            print("You've studied all the problems!")
+            keep_studying = False
         else:
-            break
+            print(f"{problem_count - index+1} problems left")
+            res = input("Press (n/N) to stop")
+            keep_studying = res.lower() != 'n' and res.lower() != 'no'
+            if keep_studying:
+                clear_terminal_screen()
+            else:
+                break
 
+if __name__ == '__main__':
+    main()
